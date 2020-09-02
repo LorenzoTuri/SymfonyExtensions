37d95afae6d6b795cf7939a94ce137719654b8c6
Lorenzo Turi <lorenzoturi1994@gmail.com>
2020-09-02 14:57:01 +0200

    Fail. Wrong exception used

--- Exceptions/UnauthorizedUserException.php
+++ Exceptions/UnauthorizedUserException.php
@@ -2,6 +2,12 @@
 
 namespace Lturi\SymfonyExtensions\Exceptions;
 
-class UnauthorizedUserException extends \Exception {
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
 
+class UnauthorizedUserException extends HttpException {
+    public function __construct (string $message = null, \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    {
+        parent::__construct(401, $message, $previous, $headers, $code);
+    }
 }
